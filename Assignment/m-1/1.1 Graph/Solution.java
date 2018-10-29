7bdc5657c75f9bfcd90f7154e9700488e41f20f3
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-29 17:25:30 +0530

    "ads2_m1_a1_1: 2/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m-1/1.1 Graph/Solution.java
+++ Assignment/m-1/1.1 Graph/Solution.java
@@ -10,32 +10,35 @@ class Solution {
 		HashMap<Integer, String> map = new HashMap<>();
 		AdjMatrixGraph matrix = new AdjMatrixGraph(vertices);
 		ListGraph graph = new ListGraph(vertices);
-		if(vertices == 0) {
+		if (vertices == 0) {
+			System.out.println(vertices + " vertices, " + edges + " edges");
 			System.out.println("No edges");
 			return;
 		} else if (edges == 0) {
+			System.out.println(vertices + " vertices, " + edges + " edges");
 			System.out.println("No edges");
 			return;
 		} else if (vertices == 0 && edges == 0) {
+			System.out.println(vertices + " vertices, " + edges + " edges");
 			System.out.println("No edges");
 			return;
 		} else {
-		String[] cities = scan.nextLine().split(",");
-		if (type.equals("List")) {
-			for (int i = 0; i < edges; i++) {
-				String[] vert = scan.nextLine().split(" ");
-				map.put(i, cities[i]);
-				graph.addEdge(Integer.parseInt(vert[0]), Integer.parseInt(vert[1]));
+			String[] cities = scan.nextLine().split(",");
+			if (type.equals("List")) {
+				for (int i = 0; i < edges; i++) {
+					String[] vert = scan.nextLine().split(" ");
+					map.put(i, cities[i]);
+					graph.addEdge(Integer.parseInt(vert[0]), Integer.parseInt(vert[1]));
+				}
+				String s = graph.toString();
+			} else if (type.equals("Matrix")) {
+				for (int i = 0; i < edges; i++) {
+					String[] vert = scan.nextLine().split(" ");
+					matrix.addEdge(Integer.parseInt(vert[0]), Integer.parseInt(vert[1]));
+					map.put(i, cities[i]);
+				}
+				String s = matrix.toString();
 			}
-			String s = graph.toString();
-		} else if (type.equals("Matrix")) {
-			for (int i = 0; i < edges; i++) {
-				String[] vert = scan.nextLine().split(" ");
-				matrix.addEdge(Integer.parseInt(vert[0]), Integer.parseInt(vert[1]));
-				map.put(i, cities[i]);
-			}
-			String s = matrix.toString();
-		}
 		}
 	}
 }
