4c8fb435ae62d7114c4d7880e3f630b7e2fb7811
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-04 23:28:05 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ Assignment/m-2/2.1 CC_Percolation/Percolation.java
@@ -0,0 +1,64 @@
+/**
+ * Class for percolation.
+ */
+class Percolation {
+    /**
+     * boolean string 2d array.
+     */
+    private boolean[][] grid;
+    private Uf uf;
+    private int size;
+    public Percolation(final int n) { // create n-by-n grid, with all sites blocked.
+        grid = new boolean[n][n];
+        uf = new Uf(n * n + 2);  // to create the one dimension array of size +
+        size = n;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(final int row, final int col) { // open site (row, col) if it is not open already
+        if (grid[row][col] == false) {
+            grid[row][col] = true;
+        }
+        if (row == 0) {
+            uf.union(convert1D(row, col), size * size);
+        } 
+        if (row == size - 1) {
+            uf.union(convert1D(row, col), size * size + 1);
+        }
+        if (row > 0 && grid[row - 1][col]) {    //top element.
+            uf.union(convert1D(row, col), convert1D(row - 1, col));
+        }
+        if (row < size - 1 && grid[row + 1][col]) { //bottom element.
+            uf.union(convert1D(row, col), convert1D(row + 1, col));
+        }
+        if (col > 0 && grid[row][col - 1]) {    //left element.
+            uf.union(convert1D(row, col), convert1D(row, col - 1));
+        }
+        if (col < size - 1 && grid[row][col + 1]) { //right element.
+            uf.union(convert1D(row, col), convert1D(row, col + 1));
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int convert1D(final int row, final int col) {
+        return row * size + col;
+    }
+    /**
+     * {}.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean percolates() {             // does the system percolate?
+        return uf.connected(size * size, size * size + 1);
+    }
+}
