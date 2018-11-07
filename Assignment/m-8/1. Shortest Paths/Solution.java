5cf9850225e9b645da00c5a481fd1ed91c16869b
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-07 13:47:49 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/m-8/1. Shortest Paths/Solution.java
+++ Assignment/m-8/1. Shortest Paths/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.HashMap;
+import java.util.ArrayList;
 /**.
  * { item_description }
  */
@@ -16,11 +16,11 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner in = new Scanner(System.in);
         String[] rail = in.nextLine().split(" ");
-        HashMap<Integer, String> stationName =
-            new HashMap<Integer, String>();
+        ArrayList<String> stationName =
+            new ArrayList<String>();
         String[] station = in.nextLine().split(" ");
         for (int i = 0; i < Integer.parseInt(rail[0]); i++) {
-            stationName.put(i, station[i]);
+            stationName.add(station[i]);
         }
         EdgeWeightedGraph edge = new EdgeWeightedGraph(
             Integer.parseInt(rail[0]));
@@ -28,17 +28,8 @@ public final class Solution {
         // System.out.println("\nKEy Set "+ a);
         for (int i = 0; i < Integer.parseInt(rail[1]); i++) {
             String[] c = in.nextLine().split(" ");
-            int vert1 = 0;
-            int vert2 = 0;
-            for (int j = 0; j < Integer.parseInt(
-                        rail[0]); j++) {
-                if (stationName.get(j).equals(c[0])) {
-                    vert1 = j;
-                }
-                if (stationName.get(j).equals(c[1])) {
-                    vert2 = j;
-                }
-            }
+            int vert1 = stationName.indexOf(c[0]);
+            int vert2 = stationName.indexOf(c[1]);
             edge.addEdge(new Edge(
                              vert1, vert2, Double.parseDouble(
                                  c[2])));
@@ -47,16 +38,8 @@ public final class Solution {
         DijkstraSP dj;
         for (int i = 0; i < queries; i++) {
             String[] q = in.nextLine().split(" ");
-            int source = 0;
-            int destination = 0;
-            for (int j = 0; j < stationName.size(); j++) {
-                if (stationName.get(j).equals(q[0])) {
-                    source = j;
-                }
-                if (stationName.get(j).equals(q[1])) {
-                    destination = j;
-                }
-            }
+            int source = stationName.indexOf(q[0]);
+            int destination = stationName.indexOf(q[1]);
             dj = new DijkstraSP(edge, source);
             System.out.println((int) dj.distTo(destination));
         }
