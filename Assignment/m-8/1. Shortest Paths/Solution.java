80aa87364d7c8088b48515df8218879be76431de
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-07 13:33:58 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/m-8/1. Shortest Paths/Solution.java
+++ Assignment/m-8/1. Shortest Paths/Solution.java
@@ -1,64 +1,64 @@
 import java.util.Scanner;
 import java.util.HashMap;
-/**
+/**.
  * { item_description }
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() { }
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner in = new Scanner(System.in);
-		String[] rail = in.nextLine().split(" ");
-		HashMap<Integer, String> stationName =
-		    new HashMap<Integer, String>();
-		String[] station = in.nextLine().split(" ");
-		for (int i = 0; i < Integer.parseInt(rail[0]); i++) {
-			stationName.put(i, station[i]);
-		}
-		EdgeWeightedGraph edge = new EdgeWeightedGraph(
-		    Integer.parseInt(rail[0]));
-		// Set<Integer> a = stationName.keySet();
-		// System.out.println("\nKEy Set "+ a);
-		for (int i = 0; i < Integer.parseInt(rail[1]); i++) {
-			String[] c = in.nextLine().split(" ");
-			int vert1 = 0;
-			int vert2 = 0;
-			for (int j = 0; j < Integer.parseInt(
-			            rail[0]); j++) {
-				if (stationName.get(j).equals(c[0])) {
-					vert1 = j;
-				}
-				if (stationName.get(j).equals(c[1])) {
-					vert2 = j;
-				}
-			}
-			edge.addEdge(new Edge(
-			                 vert1, vert2, Double.parseDouble(
-			                     c[2])));
-		}
-		int queries = Integer.parseInt(in.nextLine());
-		DijkstraSP dj;
-		for (int i = 0; i < queries; i++) {
-			String[] q = in.nextLine().split(" ");
-			int source = 0;
-			int destination = 0;
-			for (int j = 0; j < stationName.size(); j++) {
-				if (stationName.get(j).equals(q[0])) {
-					source = j;
-				}
-				if (stationName.get(j).equals(q[1])) {
-					destination = j;
-				}
-			}
-			dj = new DijkstraSP(edge, source);
-			System.out.println((int)dj.distTo(destination));
-		}
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] rail = in.nextLine().split(" ");
+        HashMap<Integer, String> stationName =
+            new HashMap<Integer, String>();
+        String[] station = in.nextLine().split(" ");
+        for (int i = 0; i < Integer.parseInt(rail[0]); i++) {
+            stationName.put(i, station[i]);
+        }
+        EdgeWeightedGraph edge = new EdgeWeightedGraph(
+            Integer.parseInt(rail[0]));
+        // Set<Integer> a = stationName.keySet();
+        // System.out.println("\nKEy Set "+ a);
+        for (int i = 0; i < Integer.parseInt(rail[1]); i++) {
+            String[] c = in.nextLine().split(" ");
+            int vert1 = 0;
+            int vert2 = 0;
+            for (int j = 0; j < Integer.parseInt(
+                        rail[0]); j++) {
+                if (stationName.get(j).equals(c[0])) {
+                    vert1 = j;
+                }
+                if (stationName.get(j).equals(c[1])) {
+                    vert2 = j;
+                }
+            }
+            edge.addEdge(new Edge(
+                             vert1, vert2, Double.parseDouble(
+                                 c[2])));
+        }
+        int queries = Integer.parseInt(in.nextLine());
+        DijkstraSP dj;
+        for (int i = 0; i < queries; i++) {
+            String[] q = in.nextLine().split(" ");
+            int source = 0;
+            int destination = 0;
+            for (int j = 0; j < stationName.size(); j++) {
+                if (stationName.get(j).equals(q[0])) {
+                    source = j;
+                }
+                if (stationName.get(j).equals(q[1])) {
+                    destination = j;
+                }
+            }
+            dj = new DijkstraSP(edge, source);
+            System.out.println((int) dj.distTo(destination));
+        }
+    }
 }
