31261d5c53d808876c00f4b0e2b5a52557fc7420
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-09 08:12:35 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m-6/ADS-2Week-1/Solution.java
+++ Assignment/m-6/ADS-2Week-1/Solution.java
@@ -16,7 +16,7 @@ class PageRank {
         dg = digraph;
         k = 0;
         newPR = new Double[dg.V()];
-        prevPR = new Double[1002][dg.V()];
+        prevPR = new Double[1000][dg.V()];
         Double firstpr = 1.0 / dg.V();
         for (int i = 0; i < dg.V(); i++) {
             newPR[i] = firstpr;
@@ -59,7 +59,7 @@ class PageRank {
     }
 
     public double getPR(int v) {
-        newPR = prevPR[1002];
+        newPR = prevPR[1000];
         return newPR[v];
     }
 
