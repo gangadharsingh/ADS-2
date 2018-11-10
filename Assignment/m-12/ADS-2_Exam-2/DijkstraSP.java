55394920ec08aa27461be2f9b6b5599b310fd45b
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-10 11:36:22 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m-12/ADS-2_Exam-2/DijkstraSP.java
+++ Assignment/m-12/ADS-2_Exam-2/DijkstraSP.java
@@ -129,7 +129,7 @@ class DijkstraSP {
         for (Edge e = edgeTo[v]; e != null; e = edgeTo[x]) {
             path.push(e);
             x = e.other(x);
-            str += (e.other(x)+1) + " ";
+            str += (e.either()+1) + " ";
         }
         return str;
     }
