804027aeb008d5ad57b46264b106b691c251f3d3
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-11-12 17:27:50 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m-9/Seam Carving/SeamCarver.java
+++ Assignment/m-9/Seam Carving/SeamCarver.java
@@ -1,37 +1,74 @@
 import java.awt.Color;
-
-
+/**.
+ * Class for seam carver.
+ */
 public class SeamCarver {
+    /**.
+     * { var_description }
+     */
     private static final double BORDER = 1000;
+    /**.
+     * { var_description }
+     */
     private Picture picture;
 
-    public SeamCarver(Picture picture) {
+    /**.
+     * Constructs the object.
+     *
+     * @param      picture  The picture
+     */
+    public SeamCarver(final Picture picture) {
         if (picture == null) {
-            throw new java.lang.IllegalArgumentException("picture is null");
+            throw new java.lang.IllegalArgumentException(
+                "picture is null");
         }
         this.picture = new Picture(picture);
     }
 
     // current picture
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Picture picture() {
         return this.picture;
     }
 
     // width of current picture
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int width() {
         return this.picture.width();
     }
 
     // height of current picture
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int height() {
         return this.picture.height();
     }
 
     // energy of pixel at column x and row y
-    public  double energy(int x, int y) {
+    /**.
+     * { function_description }
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public  double energy(final int x, final int y) {
         int w = width() - 1, h = height() - 1;
         if (x < 0 || x > w || y < 0 || y > h) {
-            throw new java.lang.IllegalArgumentException("IllegalArgumentException");
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
         }
         if (x == 0 || x == w ||  y == 0 || y == h) {
             return BORDER;
@@ -40,21 +77,43 @@ public class SeamCarver {
     }
 
     // energy of pixel at column x and row y not on boarder
-    private double internalEnergy(int x, int y) {
+    /**.
+     * { function_description }
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private double internalEnergy(final int x, final int y) {
         Color left = this.picture.get(x - 1, y);
         Color right = this.picture.get(x + 1, y);
         Color up = this.picture.get(x, y - 1);
         Color down = this.picture.get(x, y + 1);
-        return Math.sqrt(gradient(left, right) + gradient(up, down));
+        return Math.sqrt(gradient(
+                             left, right) + gradient(up, down));
     }
 
-    private double gradient(Color one, Color two) {
+    /**.
+     * { function_description }
+     *
+     * @param      one   One
+     * @param      two   Two
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private double gradient(final Color one, final Color two) {
         double red = one.getRed() - two.getRed();
         double green = one.getGreen() - two.getGreen();
         double blue = one.getBlue() - two.getBlue();
         return red * red + green * green + blue * blue;
     }
 
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     private double[][] initEnergies() {
         double[][] energies = new double[height()][width()];
         for (int i = 0; i < height(); i++) {
@@ -66,7 +125,12 @@ public class SeamCarver {
     }
 
     // pass through an array and mark the shorthest distance from top to entry
-    private void topologicalSort(double[][] energies) {
+    /**.
+     * { function_description }
+     *
+     * @param      energies  The energies
+     */
+    private void topologicalSort(final double[][] energies) {
         int h = energies.length, w = energies[0].length;
         for (int row = 1; row < h; row++) {
             for (int col = 0; col < w; col++) {
@@ -75,20 +139,30 @@ public class SeamCarver {
                 if (col == 0) {
                     min = temp;
                 } else {
-                    min = Math.min(temp, energies[row - 1][col - 1]);
+                    min = Math.min(temp,
+                                   energies[row - 1][col - 1]);
                 }
 
                 if (col != (w - 1)) {
-                    min = Math.min(min, energies[row - 1][col + 1]);
+                    min = Math.min(min,
+                                   energies[row - 1][col + 1]);
                 } else {
-                	min = min;
+                    min = min;
                 }
                 energies[row][col] += min;
             }
         }
 
     }
-    private double[][] transposeGrid(double[][] energies) {
+
+    /**.
+     * { function_description }
+     *
+     * @param      energies  The energies
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private double[][] transposeGrid(final double[][] energies) {
         int h = energies.length, w = energies[0].length;
         double[][] trEnergies = new double[w][h];
         for (int row = 0; row < h; row++) {
@@ -99,7 +173,14 @@ public class SeamCarver {
         return trEnergies;
     }
 
-    private int[] minVerticalPath(double[][] energies) {
+    /**.
+     * { function_description }
+     *
+     * @param      energies  The energies
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int[] minVerticalPath(final double[][] energies) {
         int h = energies.length, w = energies[0].length;
         int[] path = new int[h];
 
@@ -117,27 +198,55 @@ public class SeamCarver {
             int col = path[row + 1];
             // three neighboring, priority to center
             path[row] = col;
-            if (col > 0 && energies[row][col - 1] < energies[row][path[row]])
+            if (col > 0 && energies[row][col - 1]
+                    < energies[row][path[row]]) {
                 path[row] = col - 1;
-            if (col < (w - 2) && energies[row][col + 1] < energies[row][path[row]])
+            }
+            if (col < (w - 2) && energies[row][col + 1]
+                    < energies[row][path[row]]) {
                 path[row] = col + 1;
+            }
         }
         return path;
     }
+
     // sequence of indices for horizontal seam
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int[] findHorizontalSeam() {
-        double[][] transposeEnergies = transposeGrid(initEnergies());
+        double[][] transposeEnergies
+            = transposeGrid(initEnergies());
         return minVerticalPath(transposeEnergies);
     }
+
+
     // sequence of indices for vertical seam
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int[] findVerticalSeam() {
         double[][] normalEnergies = initEnergies();
         return minVerticalPath(normalEnergies);
     }
+
+
     // remove horizontal seam from picture
-    public void removeHorizontalSeam(int[] a) {
-        if (height() <= 1 || !isValid(a, width(), height() - 1))
-            throw new java.lang.IllegalArgumentException("IllegalArgumentException");
+    /**.
+     * Removes a horizontal seam.
+     *
+     * @param      a     { parameter_description }
+     */
+    public void removeHorizontalSeam(final int[] a) {
+        if (height() <= 1 || !isValid(
+                    a, width(), height() - 1)) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
         Picture pic = new Picture(width(), height() - 1);
         for (int w = 0; w < width(); w++) {
             for (int h = 0; h < a[w]; h++) {
@@ -153,16 +262,21 @@ public class SeamCarver {
     }
 
     // remove vertical seam from picture
-    public void removeVerticalSeam(int[] a) {
-        if (width() <= 1 || !isValid(a, height(), width()))
-            throw new java.lang.IllegalArgumentException("IllegalArgumentException");
+    /**.
+     * Removes a vertical seam.
+     *
+     * @param      a     { parameter_description }
+     */
+    public void removeVerticalSeam(final int[] a) {
+        if (width() <= 1 || !isValid(a, height(), width())) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
         Picture pic = new Picture(width() - 1, height());
         for (int h = 0; h < height(); h++) {
             for (int w = 0; w < a[h]; w++) {
                 pic.set(w, h, this.picture.get(w, h));
             }
-
-
             for (int w = a[h] + 1; w < width(); w++) {
                 pic.set(w - 1, h, this.picture.get(w, h));
             }
@@ -172,7 +286,16 @@ public class SeamCarver {
     }
 
     // return false if two consecutive entries differ by more than 1
-    private boolean isValid(int[] a, int len, int range) {
+    /**.
+     * Determines if valid.
+     *
+     * @param      a      { parameter_description }
+     * @param      len    The length
+     * @param      range  The range
+     *
+     * @return     True if valid, False otherwise.
+     */
+    private boolean isValid(final int[] a, final int len, final int range) {
         if (a == null) {
             return false;
         }
@@ -180,8 +303,10 @@ public class SeamCarver {
             return false;
         }
         for (int i = 1; i < len; i++) {
-            if (a[i] < Math.max(0, a[i - 1] - 1) || a[i] > Math.min(range, a[i - 1] + 1))
+            if (a[i] < Math.max(0, a[i - 1] - 1) ||
+                    a[i] > Math.min(range, a[i - 1] + 1)) {
                 return false;
+            }
         }
         return true;
     }
